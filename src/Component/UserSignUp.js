d82c58586f4617d8a238eee07f2aa62d92e4e0ed
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-27 14:24:35 +0200

    Correct API endpoint path

--- src/Component/UserSignUp.js
+++ src/Component/UserSignUp.js
@@ -28,16 +28,16 @@ function UserSignUp() {
     <>
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
         <br />
         {' '}
         <br />
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
         {' '}
         <br />
         {' '}
         <br />
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <br />
         {' '}
         <br />
