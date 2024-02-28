2f3f4a839f77e5134dd56e249974f5a52e408fc8
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-28 10:10:35 +0000

    Add UserSignUp component

---
+++ src/Components/UserSignUp.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+
+function UserSignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log('Form data:', { name, email, password });
+    try {
+      console.log('Submitting form...');
+      const response = await axios.post('http://localhost:3000/users', {
+        user: {
+          name,
+          email,
+          password,
+        },
+      });
+      console.log('Response:', response.data);
+      navigate('/');
+    } catch (error) {
+      console.log('Error:', error.response.data);
+      console.error('Axios error:', error);
+    }
+  };
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+        <br />
+        {' '}
+        <br />
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        {' '}
+        <br />
+        {' '}
+        <br />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <br />
+        {' '}
+        <br />
+        <button type="submit">Sign Up</button>
+        {' '}
+        <br />
+      </form>
+      <p><Link to="/">Sign In</Link></p>
+    </>
+  );
+}
+export default UserSignUp;
