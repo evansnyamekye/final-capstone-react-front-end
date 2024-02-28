e0c64e5f3e9a2860d2a3973e14e4e171cbab2338
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-28 10:12:53 +0000

    Remove BrowserRouter from root render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -7,7 +7,5 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <App />,
 );
