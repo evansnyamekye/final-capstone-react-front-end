6f7e69834024d76bd608b2db358a0d14e92cac25
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-28 10:13:08 +0000

    Add Layout component

---
+++ src/Components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function Layout() {
+  return (
+    <div>
+      {/* <h1>StaySpare</h1>
+             <p>This is the main Content of the Hotel application</p> */}
+      <Navigation />
+    </div>
+  );
+}
+
+export default Layout;
