f00f798ffadb344e187ebd8c6c209c339d35bdeb
George <georgie.george7@gmail.com>
2024-02-29 12:01:13 +0400

    Add Redux store and Provider to App component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './Redux/store';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
