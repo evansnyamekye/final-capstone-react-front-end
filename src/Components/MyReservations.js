63b01751a7f36bd684c156462020b1d942835aed
George <georgie.george7@gmail.com>
2024-02-29 14:53:15 +0400

    Add MyReservations component

---
+++ src/Components/MyReservations.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MyReservations() {
+  return (
+    <div>
+      {/* <h1>StaySpare</h1>
+             <p>This is the main Content of the Hotel application</p> */}
+      This is my reservations page
+    </div>
+  );
+}
+
+export default MyReservations;
