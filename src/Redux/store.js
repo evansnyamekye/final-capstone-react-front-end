47e571968559802fed9ccc3b725702b302b692c1
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-29 16:02:35 +0200

    Add addPlaceReducer to store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import placesReducer from './places/placesSlice';
+import addPlaceReducer from './places/addPlaceSlice';
 
 export default configureStore({
   reducer: {
     places: placesReducer,
+    addplace: addPlaceReducer,
   },
 });
