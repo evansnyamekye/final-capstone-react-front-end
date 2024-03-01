cf3c40d914dfe0ce8135f519c45147f41d5021e8
George <georgie.george7@gmail.com>
2024-03-01 12:50:08 +0400

    Add new SVG image and update Layout component

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navigation from './Navigation';
-import '../Navigation.css';
 
 function Layout() {
   return (
     <div className="container">
       <Navigation />
       {/* Start of Main Content */}
-      <div className="Center">
-        <h1>StaySpare</h1>
-        <Outlet />
-        <h4>Please select a hotel</h4>
-      </div>
+      <Outlet />
       {/* End of Main Content */}
 
     </div>
