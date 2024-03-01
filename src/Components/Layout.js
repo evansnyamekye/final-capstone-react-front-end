68bfb36a4691ea73dec513cc139b39b92674cc47
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-01 17:26:21 +0200

    Resolve lyout.js conflicts

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navigation from './Navigation';
-import '../Navigation.css';
 
 function Layout() {
   return (
     <div className="container">
       <Navigation />
       {/* Start of Main Content */}
-      <div className="Center">
-        {/* <h1>StaySpare</h1> */}
-        <Outlet />
-      </div>
+      <Outlet />
       {/* End of Main Content */}
 
     </div>
