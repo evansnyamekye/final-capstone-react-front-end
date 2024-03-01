e82a4bfb0699f4cdf1fb475da38d893a359345ec
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-01 16:59:19 +0200

    Import Styling component

--- src/Components/AddPlace.js
+++ src/Components/AddPlace.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../AddPlace.css';
 
 function AddPlace() {
   const [placeData, setPlaceData] = useState({
@@ -35,23 +36,26 @@ function AddPlace() {
   return (
     <div className="form-container">
       <h2>Add Place</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="description" placeholder="Description" value={placeData.description} onChange={handleChange} />
-        <input type="text" name="photo" placeholder="Photo URL" value={placeData.photo} onChange={handleChange} />
-        <input type="text" name="location" placeholder="Location" value={placeData.location} onChange={handleChange} />
-        <input type="number" name="rate" placeholder="Rate" value={placeData.rate} onChange={handleChange} />
-        <input type="text" name="address" placeholder="Address" value={placeData.address} onChange={handleChange} />
+      <input type="text" name="description" placeholder="Description" value={placeData.description} onChange={handleChange} />
+      <br />
+      <input type="text" name="photo" placeholder="Photo URL" value={placeData.photo} onChange={handleChange} />
+      <br />
+      <input type="text" name="location" placeholder="Location" value={placeData.location} onChange={handleChange} />
+      <br />
+      <input type="number" name="rate" placeholder="Rate" value={placeData.rate} onChange={handleChange} />
+      <br />
+      <input type="text" name="address" placeholder="Address" value={placeData.address} onChange={handleChange} />
+      <br />
+      {' '}
+      {/* Add address input */}
+      {status === 'failed' && (
+      <div className="error-message">
+        Error:
         {' '}
-        {/* Add address input */}
-        {status === 'failed' && (
-          <div className="error-message">
-            Error:
-            {' '}
-            {error}
-          </div>
-        )}
-        <button type="submit" disabled={status === 'loading'}>Add Place</button>
-      </form>
+        {error}
+      </div>
+      )}
+      <button type="submit" onClick={handleSubmit} disabled={status === 'loading'}>Add Place</button>
     </div>
   );
 }
