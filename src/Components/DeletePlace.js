4636df9bb3d1a05b936adc5304ff84c2800608af
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-02 14:51:57 +0200

    Refactor DeletePlace component to use Redux for deleting items

--- src/Components/DeletePlace.js
+++ src/Components/DeletePlace.js
@@ -1,9 +1,30 @@
-function DeletePlace() {
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteItem } from '../Redux/places/deleteItemSlice';
+import '../DeleteItem.css';
+
+const DeletePlace = () => {
+  const dispatch = useDispatch();
+  const places = useSelector((state) => state.places.places);
+
+  const handleDelete = (placeId) => {
+    dispatch(deleteItem(placeId));
+  };
+
   return (
-    <div>
-      <h2>Delete Place</h2>
+    <div className="center">
+      <h1>Delete Place</h1>
+      <ul>
+        {places.map((place) => (
+          <li key={place.id} className="card">
+            <h3>{place.description}</h3>
+            <img className="place-img" src={place.photo} alt={place.description} />
+            <button className="delete" type="submit" onClick={() => handleDelete(place.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default DeletePlace;
