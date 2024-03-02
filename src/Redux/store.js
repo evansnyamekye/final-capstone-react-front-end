fbeda58168e57cf65ce751dd9f413471dc622bc8
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-02 12:03:07 +0200

    Add deleteItemReducer to store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import placesReducer from './places/placesSlice';
 import addPlaceReducer from './places/addPlaceSlice';
+import deleteItemReducer from './places/deleteItemSlice';
 
 export default configureStore({
   reducer: {
     places: placesReducer,
     addPlace: addPlaceReducer,
+    deleteItem: deleteItemReducer,
+
   },
 });
