b23eacb8a42bebc754f4f493ff8ef0c52b92671e
George <georgie.george7@gmail.com>
2024-03-02 17:24:39 +0400

    Add myReservationsSlice to store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import placesReducer from './places/placesSlice';
 import addPlaceReducer from './places/addPlaceSlice';
+import reservationsReducer from './reservations/myReservationsSlice';
 
 export default configureStore({
   reducer: {
     places: placesReducer,
     addPlace: addPlaceReducer,
+    myReservations: reservationsReducer,
   },
 });
