7529c96e8c6367f4973f2b721fc468045a429e19
George <georgie.george7@gmail.com>
2024-03-06 23:30:52 +0400

    Refactor Layout component

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,16 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navigation from './Navigation';
 
-function Layout() {
-  return (
-    <div className="container">
-      <Navigation />
-      {/* Start of Main Content */}
-      <Outlet />
-      {/* End of Main Content */}
-
-    </div>
-  );
-}
+const Layout = () => (
+  <div className="container">
+    <Navigation />
+    {/* Start of Main Content */}
+    <Outlet />
+    {/* End of Main Content */}
+  </div>
+);
 
 export default Layout;
